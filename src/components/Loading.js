191afd85e8100aefd1b22e966b86f80b5d965b82
joshua <joshares9991@gmail.com>
2023-07-29 16:47:42 +0100

    map loading state

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -6,7 +6,7 @@ const Loading = () => {
       <div role="status">
         <svg
           aria-hidden="true"
-          className="inline w-24 h-24 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          className="inline w-24 h-24 mr-2 text-black animate-spin dark:text-black fill-[#ab55ef]"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
